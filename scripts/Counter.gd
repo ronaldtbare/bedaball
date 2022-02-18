1d2d21cff0d1cc244764705d12bb00c2e0078c03
Travis Bare <44512283+ronaldtbare@users.noreply.github.com>
2022-02-18 14:41:45 -0600

    more7

---
+++ scripts/Counter.gd
@@ -0,0 +1,17 @@
+extends Label
+
+var coins = 0
+
+
+func _ready():
+	text = String(coins)
+
+
+func _on_coin_coinCollected():
+	coins = coins + 1
+	text = String(coins)
+	if coins == 10:
+		$Timer.start()
+
+func _on_Timer_timeout():
+	get_tree().change_scene("res://YouWin.tscn")
